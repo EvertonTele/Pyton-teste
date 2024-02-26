35a8ded56d3ab29340bf5b528bd0b18494ed4eeb
EvertonTele <139225703+EvertonTele@users.noreply.github.com>
2024-02-26 15:09:02 -0300

    Initial commit
    pythont
    
    Create app.py

---
+++ aula8a/app.py
@@ -0,0 +1,14 @@
+# import math
+# num = int(input('Digite um numero '))
+# raiz = math.sqrt(num)
+# print ('A raiza de {} é igual a {}'.format(num, math.ceil(raiz)))
+# # para arredondar para baixo, basta utilizar floor, segue exemplo: format(num, math.floor(raiz)))
+    
+    
+# #from math import sqrt, floor
+# num = int(input('Digite um numero '))
+# raiz = sqrt(num)
+# print ('A raiza de {} é igual a {}'.format(num, raiz))
+    
+import emoji 
+print(emoji.emojize('Olá mundo :sunglasses:', use_aliases=True))
---
+++ aula9a/app.py
@@ -0,0 +1,37 @@
+# frase = 'Curso em video Python'
+# print(frase[3:13])
+
+# frase = 'Curso em Video Python'
+# print(frase[:13])
+
+# pular de 2 em 2  
+# frase = 'Curso em Video Python'
+# print(frase[1:15:2]) 
+# # pular de 2 em 2  
+
+# # não sei o inicio não sei o final pulando de 2 em 2 
+# frase = 'Curso em Video Python'
+# print(frase[::2]) 
+ 
+# # contagem dos caracteres
+# frase = 'Curso em Video Python'
+# print(frase.count('o')) 
+
+# # colocando as letras em maiusculo e contar quantos 'o maiusculo tem '
+# frase = 'Curso em Video Python'
+# print(frase.upper().count('O'))
+
+# A função len vê o tamanho da frase o espaço também conta. 
+# frase = 'Curso em Video Python'
+# print(len(frase)) 
+
+# strip tira os espaços indesejados.  
+# frase = '   Curso em Video Python   '
+# print(len(frase.strip() 
+
+# # Trocando o nome de Python para Android
+# frase = 'Curso em Video Python'
+# print(frase.replace('Python','Android'))
+
+
+
